942b9fcf91ff918f9e7d2afd7f3cf06c07508135
AsA <102066638+syrabrox@users.noreply.github.com>
2024-11-12 16:50:31 +0100

    Update knowunity.code.js

--- knowunity.code.js
+++ knowunity.code.js
@@ -1,12 +1,14 @@
-//(function() {
-   // 'use strict';
+(function() {
+    'use strict';
+
     if (window.location.href.includes("knowunity.de/knows/search")) {
         return;
     }
+
     if (window.location.href.includes("knowunity.de/knows/")) {
         if (confirm("Do you want to download this Knowunity page?")) {
             const knowunityUrl = encodeURIComponent(window.location.href);
             window.location.href = `https://cr4ck.de/knowunity.html?link=${knowunityUrl}`;
         }
     }
-//})();
+})();
