3d7d5d467704b1704d2a7187acbe5b1a5eff7115
AsA <102066638+syrabrox@users.noreply.github.com>
2024-11-12 16:37:11 +0100

    Update knowunity.code.js

--- knowunity.code.js
+++ knowunity.code.js
@@ -1,16 +1,10 @@
 //(function() {
    // 'use strict';
-
-    // Check if on a search page (do nothing if true)
     if (window.location.href.includes("knowunity.de/knows/search")) {
-        return; // Exit script if on search page
+        return;
     }
-
-    // Check if on a specific knowunity.de/knows/{xy} page
     if (window.location.href.includes("knowunity.de/knows/")) {
-        // Prompt the user for download confirmation
         if (confirm("Do you want to download this Knowunity page?")) {
-            // Redirect to cr4ck.de with the current URL as a parameter
             const knowunityUrl = encodeURIComponent(window.location.href);
             window.location.href = `https://cr4ck.de/knowunity.html?link=${knowunityUrl}`;
         }
