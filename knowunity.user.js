3139f69a18c7294a462c6a771e36df26e88d873a
AsA <102066638+syrabrox@users.noreply.github.com>
2024-11-12 10:42:54 +0100

    Update knowunity.user.js

--- knowunity.user.js
+++ knowunity.user.js
@@ -1,27 +1,12 @@
 // ==UserScript==
-// @name         Autofill on mail.cr4ck.de
+// @name         Knowunity Downloader
 // @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Autofill inputs on mail.cr4ck.de
-// @homepageURL  https://cr4ck.de
-// @supportURL   https://cr4ck.de/dc
-
-// Add all new websites here
-// @match        https://mail.cr4ck.de/*
-
-// @license      CC BY-NC 4.0 (https://creativecommons.org/licenses/by-nc/4.0/).
-
-// @downloadURL  https://cr4ck.de/userscript
-// @updateURL    https://cr4ck.de/userscript
-
-// @grant        GM_xmlhttpRequest
-// @grant        GM.xmlHttpRequest
-
-// @version      1.0.0
-// @releaseDate  2024-09-02T05:23:50.212Z
-// @author       cr4ck.de team
-// @connect      raw.githubusercontent.com
-
+// @version      1.1
+// @description  Redirect to download page with URL input
+// @match        *://knowunity.de/knows/*
+// @match        https://cr4ck.de/knowunity
+// @match        https://cr4ck.de/knowunity.html
+// @grant        none
 // ==/UserScript==
 
 (function() {
